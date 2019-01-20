92119bdf4e4d8695e102a084857d2a23e55bf7dc
cdlysoo <930624046@qqcom>
2019-01-20 14:47:01 +0800

    完成注册页面

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 //导包
 const express = require('express')
+const path = require('path')
 
 //创建app
 const app = express()
 
+app.use(express.static(path.join(__dirname,"public")))
 
 //处理请求
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+const accountRouter = require(path.join(__dirname,"./routers/accountRouter.js"))
+app.use('/account',accountRouter)
 
 
 //启动
---
+++ src/controllers/accountController.js
@@ -0,0 +1,10 @@
+/**
+ * 导出一个方法,该方法获取注册页面
+ */
+const path = require('path')
+
+
+exports.getRegisterPage = (req,res) => {
+    res.sendFile(path.join(__dirname,"../public/views/register.html"))
+}
+
